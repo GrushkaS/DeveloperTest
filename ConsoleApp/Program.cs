73e6aeefc637501c5b379e09e4908739fe87dd73
GrushkaS <vadimdrach600@gmail.com>
2023-07-21 16:07:21 +0300

    Init solution

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, ConsoleApp!");
+
+
+
+
+            Console.WriteLine("Task ended!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Task3!");
+
+
+
+
+            Console.WriteLine("Task ended!");
+            Console.ReadKey();
+        }
+    }
+}
